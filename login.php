6da85228af0e23054ffdd2d53f2cec20191dc8a6
darroyo606 <dcarroyop@gmail.com>
2020-11-27 15:14:48 -0500

    framework

--- login.php
+++ login.php
@@ -6,12 +6,12 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<link rel="stylesheet" href="assets/css/main.css" />
 	</head>
-	<body class="single is-preload">
+	<body class="single is-preload" style="background-color:#c4d2e7;">
 
 <div id="wrapper">
 
 				<!-- Header -->
-					<header id="header" style="background-color:black;" >
+					<header id="header" style="background-color:#789dca;" >
 						<h1  ><a>Futuro Imperfecto Answers</a></h1>
 
 						<nav class="links">
@@ -41,7 +41,7 @@
                 
                 <!-- Footer -->
                 <section id="footer" class="final">
-                  <p class="copyright" style="color:white">&copy; Arroyo - Arteaga - Guanuche - López </a>.  -- "Proyecto Final" -- </a>.</p>
+                  <p class="copyright" style="color:white"><font size="3">&copy; Arroyo - Arteaga - Guanuche - López </a>.  -- "Proyecto Final" -- </a>.</font></p>
                 </section>
 
             </div>
