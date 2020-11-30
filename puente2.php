a7d1b9c26c749c99ef3509a400ff53ccaa085c14
alx08 <49490819+alx08@users.noreply.github.com>
2020-11-29 21:36:47 -0500

    puntuacion

---
+++ puente2.php
@@ -0,0 +1,23 @@
+<?php 
+session_start();
+include("functionsco.php");
+isset($_POST['idresp']);
+{
+    $respuesta=$_POST['idresp'];
+}
+isset($_POST['idpreg']);
+{
+    $pregunta=$_POST['idpreg'];
+}
+
+Conectarco();
+$IDUSUARIO=$_SESSION['usuarioactivo'];
+
+$sql = "UPDATE respuesta SET ESTADORESPUESTA=ESTADORESPUESTA+1 WHERE IDRESPUESTA=$respuesta"; //selecciona la id de la pregunta perteneciente a la categoria
+$idpre = $conn->query($sql);
+//fin de verificacion de respuestas
+
+Desconectarco();
+header("Location: ppregunta.php?var=$pregunta"); 
+
+?>
--- respuesta.php
+++ respuesta.php
@@ -10,15 +10,17 @@ $cont = $conn->query($sql);
 $cont->execute(); 
 $number_of_rows = $cont->fetchColumn(); //Numero de Categorias
 
-$sql = "SELECT IDUSUARIO, DESCRIPCIONRESPUESTA,FECHACREACIONRESPUESTA1 FROM respuesta WHERE respuesta.IDPREGUNTA = $idpregunta"; //separa las categorias
+$sql = "SELECT IDUSUARIO, DESCRIPCIONRESPUESTA,FECHACREACIONRESPUESTA1, IDRESPUESTA FROM respuesta WHERE respuesta.IDPREGUNTA = $idpregunta"; //separa las categorias
 $res = $conn->query($sql);
 
 foreach ($res as $fila) {
     $usu[$k] = $fila["IDUSUARIO"]; //almacena la respuesta
     $resp[$k] = $fila["DESCRIPCIONRESPUESTA"]; //alamacena el usuario 
     $fecha[$k] = $fila["FECHACREACIONRESPUESTA1"]; //almacena la fecha
+    $idresp[$k] = $fila["IDRESPUESTA"]; //almacena la id de respuesta
     $k++;
-}
+}	
+
 Desconectarco();
 
 
@@ -31,9 +33,14 @@ if($number_of_rows>0) //verificacion de la existenca de respuestas
     echo("
             <div class=\"post\" style=\"border-radius:10px\">
             <h1>$usu[$i]</h1>
-                <form name=\"form\"  id=\"form\" method=\"POST\">$resp[$i]       
+                <form name=\"form\" action=\"puente2.php\" id=\"form\" method=\"POST\">$resp[$i]       
                 </form>
-                <form class=\"mini-post\" name=\"form\"  id=\"form\" method=\"POST\">$fecha[$i]       
+                <form class=\"mini-post\" name=\"form\"  id=\"form\" method=\"POST\">$fecha[$i]    
+                <input id=\"prodId2\" name=\"idresp\" value=".$idresp[$i]." type=\"hidden\">   
+                <input id=\"prodId\" name=\"idpreg\" value=".$idpregunta." type=\"hidden\">  
+                <input type=\"submit\" name=\"idpreg1\" value=\"Votar\">
+                </form>
+
                 </form>
             </div>
 
